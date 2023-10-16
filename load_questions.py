e1b0df5eba2aaa5f05dda32a18bbe237d4745fb4
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-10-16 19:36:49 +0300

    update load questions for redis

--- load_questions.py
+++ load_questions.py
@@ -1,29 +1,56 @@
 import os
+import json
+
+import redis
+from environs import Env
 
 
-def extract_questions(text):
-    questions = {}
-    question, answer = None, None
-
-    for contents in text:
-        if 'Вопрос' in contents:
-            question = ' '.join(contents.strip().splitlines()[1:])
-        elif 'Ответ' in contents:
-            answer = ' '.join(contents.strip().splitlines()[1:])
-        if question and answer:
-            questions[question] = answer
-
-    return questions
-
-
-def load_questions(directory='questions'):
-    all_questions = {}
-
+def read_files(directory='quiz-questions', file_encoding='KOI8-R'):
     for file in os.listdir(os.path.abspath(directory)):
-        with open(f'{directory}/{file}', 'r', encoding='KOI8-R') as file:
-            text = file.read().split('\n\n')
+        with open(f'{directory}/{file}', 'r', encoding=file_encoding) as file:
+            text = file.read()
+            yield text
 
-        questions = extract_questions(text)
-        all_questions.update(questions)
 
-    return all_questions
+def redis_load_question(redis_conn, hash_name='questions'):
+    for text in read_files():
+        counter = redis_conn.hlen(hash_name)
+
+        sections_text = text.split('\n\n')
+
+        for section in sections_text:
+            if section.strip().startswith('Вопрос'):
+                question = ' '.join(section.strip().splitlines()[1:])
+                continue
+
+            if section.strip().startswith('Ответ'):
+                answer = ' '.join(section.strip().splitlines()[1:])
+                counter += 1
+                redis_conn.hset(
+                    hash_name,
+                    f'question_{counter}',
+                    json.dumps({'question': question, 'answer': answer})
+                )
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    redis_host = env('REDIS_HOST')
+    redis_port = env('REDIS_PORT')
+    redis_password = env('REDIS_PASSWORD')
+
+    redis_conn = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password,
+        charset='utf-8',
+        decode_responses=True
+    )
+
+    redis_load_question(redis_conn)
+
+
+if __name__ == '__main__':
+    main()
